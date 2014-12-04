813596f05594e828838934245a56066613138464
Juan Carlos Chavez Gallardo <truenorobot>
2014-12-04 09:51:13 -0600

    Base del proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrador
+ * Date: 04/12/2014
+ * Time: 9:43
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ProyectoFin
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
